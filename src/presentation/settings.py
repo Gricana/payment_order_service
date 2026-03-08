9c19654facbd71c9b5a3cd5bbc765b84bb5fb133
Gricana <alex2904.goryachev@yandex.com>
2026-03-08 19:36:01 +0300

    ci: fix log config

--- src/presentation/settings.py
+++ src/presentation/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     database_url: str = "postgresql+asyncpg://postgres:postgres@db:5432/payments"
     bank_api_base_url: str = "http://bank-api:8000"
     bank_api_mode: str = "fake"
-    logs_dir: str = "/tmp/logs"
+    logs_dir: str = "/tmp/logs"  # nosec B108
 
 
 settings = Settings()
